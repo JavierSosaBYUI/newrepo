e908c50c743e3218d379302f8e35769176df76be
JAVIER SOSA <sos17001@byui.edu>
2024-05-28 21:21:24 -0400

    MVC Getting Started - w06

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -25,9 +26,11 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", function(req, res){
+app.get("/", baseController.buildHome), function(req, res){
   res.render("index", {title: "Home"})
-})
+}
+
+// Checking line 29; if this is right app.get("/", function(req, res){
 
 /* ***********************
  * Local Server Information
