d217b27a099cceab6c028073341f5ec75c79e77f
JAVIER SOSA <sos17001@byui.edu>
2024-07-08 10:35:30 -0400

    Account Management

--- database/cse340_rebuild.sql
+++ database/cse340_rebuild.sql
@@ -1,7 +1,3 @@
-/*
-  Database: 'cse 340 project', .public schema
-  Assignment 2, Task 2
-*/
 -- Create client_type data type
 DROP TYPE IF EXISTS public.account_type;
 CREATE TYPE public.account_type AS ENUM
@@ -241,53 +237,11 @@ VALUES   (
     5
   );
 
--- 5.2 Modify the Tony Stark record to change the account_type to "Admin".
-
-UPDATE public.account 
-SET account_type = 'Admin'
-WHERE account_firstname = 'Tony';
-
--- 5.3 Delete the Tony Stark record from the database.
-
-DELETE FROM public.account
-WHERE account_firstname = 'Tony';
-
--- 5.4.1 Modify the "GM Hummer" record to read "a huge interior" rather than "small interiors" using a single query
-
---(Do NOT retype the entire description as part of the query)
-/* UPDATE public.inventory 
-SET inv_description = 'Do you have 6 kids and like to go offroading? The Hummer gives you a huge interior with an engine to get you out of any muddy or rocky situation'
-WHERE inv_make = 'GM'; */
-
---5.4.2 Using REPLACE
-
+-- 4. Replace data for table 'public.inventory'
 UPDATE public.inventory
-SET inv_description = REPLACE (inv_description, 'small interiors', 'a huge interior');
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior');
 
---replacing the article 'the' before the description fragment 'a huge interior'
+-- 6. Update all records in the inventory table to add "/vehicles"
 UPDATE public.inventory
-SET inv_description = REPLACE (inv_description, 'the a huge interior', 'a huge interior');
-
-
--- 5.5 JOIN SQL 
-/* Use an inner join to select the make and model fields from the inventory table 
-   and the classification name field from the classification table for inventory items 
-   that belong to the "Sport" category */
-
-SELECT
-i.classification_id, 
-i.inv_make, 
-i.inv_model
-FROM public.inventory i
-INNER JOIN public.classification c
-	ON c.classification_id = i.classification_id
-	WHERE c.classification_id = 2;
-
--- 5.6 
-/*Update all records in the inventory table to add "/vehicles" 
-to the middle of the file path in the inv_image and inv_thumbnail columns using a single query */
-
-UPDATE public.inventory
-SET 
-inv_image = REPLACE (inv_image, '/images', '/images/vehicles'),
-inv_thumbnail = REPLACE (inv_thumbnail, '/images', '/images/vehicles');
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
